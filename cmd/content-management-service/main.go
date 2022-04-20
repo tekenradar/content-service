5b410b4b9abad6419b82cd5097a83e238575d43b
Katja Schneider <katja.schneider@coneno.com>
2022-04-20 14:29:30 +0200

    Add and test DB method to find all entries that are newer than a timestamp

--- cmd/content-management-service/main.go
+++ cmd/content-management-service/main.go
@@ -1,13 +1,15 @@
 package main
 
 import (
+	"fmt"
 	"strconv"
+
 	//"time"
 
 	"github.com/coneno/logger"
 	"github.com/tekenradar/content-service/pkg/dbs/contentdb"
-	"github.com/tekenradar/content-service/pkg/types"
-	"go.mongodb.org/mongo-driver/bson/primitive"
+	//"github.com/tekenradar/content-service/pkg/types"
+	//"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 func main() {
@@ -19,13 +21,13 @@ func main() {
 	contentDBService := contentdb.NewContentDBService(conf.ContentDBConfig)
 
 	logger.Debug.Println(contentDBService)
-
+/*
 	testMapData := types.MapData{
-		Time: 12323123123,
+		Time: 12323000000, //12323123123,
 		Lng: 21.232332,
 		Lat: 5.34534,
 		Type: "TB",
-	}
+	}*/
 	
 	//var (
 	//testInstanceID = strconv.FormatInt(time.Now().Unix(), 10)
@@ -33,13 +35,19 @@ func main() {
 
 	testInstanceID := strconv.FormatInt(1650359785,10)
 
-	id, err := contentDBService.AddMapData(testInstanceID, testMapData)
+	//id, err := contentDBService.AddMapData(testInstanceID, testMapData)
+
+	mapData, err := contentDBService.FindMapDataByTime(testInstanceID, 12323100000)
 
 	if err != nil {
 		logger.Error.Printf("unexpected error: %s", err.Error())
 	}
-	if len(id) < 2 || id == primitive.NilObjectID.Hex() {
-		logger.Error.Printf("unexpected id: %s", id)
+	//if len(id) < 2 || id == primitive.NilObjectID.Hex() {
+	//	logger.Error.Printf("unexpected id: %s", id)
+	//} 
+
+	for _, el := range mapData {
+	fmt.Printf("Time: %v\tLatitude: %v\n",el.Time, el.Lat)
 	}
 
 }
--- pkg/dbs/contentdb/db_map-infos.go
+++ pkg/dbs/contentdb/db_map-infos.go
@@ -2,7 +2,9 @@ package contentdb
 
 import (
 	"github.com/tekenradar/content-service/pkg/types"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func (dbService *ContentDBService) AddMapData(instanceID string, mapData types.MapData) (string, error) {
@@ -12,4 +14,43 @@ func (dbService *ContentDBService) AddMapData(instanceID string, mapData types.M
 	res, err := dbService.collectionRefStudyInfos(instanceID).InsertOne(ctx, mapData)
 	id := res.InsertedID.(primitive.ObjectID)
 	return id.Hex(), err
+}
+
+
+func (dbService *ContentDBService) FindMapDataByTime(instanceID string, time int64) (mapData []types.MapData, err error){
+	ctx, cancel := dbService.getContext()
+	defer cancel()
+
+	filter := bson.M{}
+	if time > 0 {
+		filter["time"] = bson.M{"$gt": time}
+	}
+
+	batchSize := int32(32)
+	opts := options.FindOptions{
+		BatchSize: &batchSize,
+	}
+	cur, err := dbService.collectionRefStudyInfos(instanceID).Find(ctx,filter, &opts)
+
+	if err != nil {
+		return mapData, err
+	}
+	defer cur.Close(ctx)
+
+	mapData = []types.MapData{}
+	for cur.Next(ctx) {
+		var result types.MapData
+		err := cur.Decode(&result)
+
+		if err != nil {
+			return mapData, err
+		}
+
+		mapData = append(mapData, result)
+	}
+	if err := cur.Err(); err != nil {
+		return mapData, err
+	}
+
+	return mapData, nil
 }
