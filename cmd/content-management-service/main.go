e25fc96cbea5c01cd1e54e1f42a19eb4a690d7ad
Katja Schneider <katja.schneider@coneno.com>
2022-04-21 10:57:56 +0200

    Update main.go

--- cmd/content-management-service/main.go
+++ cmd/content-management-service/main.go
@@ -8,8 +8,8 @@ import (
 
 	"github.com/coneno/logger"
 	"github.com/tekenradar/content-service/pkg/dbs/contentdb"
-	//"github.com/tekenradar/content-service/pkg/types"
-	//"go.mongodb.org/mongo-driver/bson/primitive"
+	"github.com/tekenradar/content-service/pkg/types"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 func main() {
@@ -21,13 +21,13 @@ func main() {
 	contentDBService := contentdb.NewContentDBService(conf.ContentDBConfig)
 
 	logger.Debug.Println(contentDBService)
-/*
+
 	testMapData := types.MapData{
-		Time: 12323000000, //12323123123,
-		Lng: 21.232332,
-		Lat: 5.34534,
+		Time: 1650530469, 
+		Lng: 21.262332,
+		Lat: 6.34534,
 		Type: "TB",
-	}*/
+	}
 	
 	//var (
 	//testInstanceID = strconv.FormatInt(time.Now().Unix(), 10)
@@ -35,19 +35,23 @@ func main() {
 
 	testInstanceID := strconv.FormatInt(1650359785,10)
 
-	//id, err := contentDBService.AddMapData(testInstanceID, testMapData)
+	id, err := contentDBService.AddMapData(testInstanceID, testMapData)
+
+	if err != nil {
+		logger.Error.Printf("unexpected error: %s", err.Error())
+	}
+	if len(id) < 2 || id == primitive.NilObjectID.Hex() {
+		logger.Error.Printf("unexpected id: %s", id)
+	} 
 
 	mapData, err := contentDBService.FindMapDataByTime(testInstanceID, 12323100000)
 
 	if err != nil {
 		logger.Error.Printf("unexpected error: %s", err.Error())
 	}
-	//if len(id) < 2 || id == primitive.NilObjectID.Hex() {
-	//	logger.Error.Printf("unexpected id: %s", id)
-	//} 
 
 	for _, el := range mapData {
-	fmt.Printf("Time: %v\tLatitude: %v\n",el.Time, el.Lat)
+	fmt.Printf("Time: %v\tLatitude: %v\tLongitude: %v\n",el.Time, el.Lat, el.Lng)
 	}
 
 }
