222b71ffcde98e4f30cdd26f61ea72531296fa3a
Katja Schneider <katja.schneider@coneno.com>
2022-05-09 10:33:12 +0200

    Save body of request as json file

--- pkg/http/v1/content-management-api.go
+++ pkg/http/v1/content-management-api.go
@@ -1,6 +1,11 @@
 package v1
 
 import (
+	"fmt"
+	"io/ioutil"
+
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 	mw "github.com/tekenradar/content-service/pkg/http/middlewares"
 )
@@ -14,10 +19,40 @@ func (h *HttpEndpoints) AddContentManagementAPI(rg *gin.RouterGroup) {
 }
 
 func (h *HttpEndpoints) addTBReportHandl(c *gin.Context) {
-	// TODO: for debugging, sve POST body as a JSON
-	// TODO: POST body -> TBmapdata
+	
 
-	// id, err := h.contentDB.AddTickBiteMapData(...)
+	// TODO: for debugging, save POST body as a JSON
+	resp, err := ioutil.ReadAll(c.Request.Body)
+
+    if err != nil {
+        c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
+            "message": "Unable to read request body",
+        })
+        return
+    }
+
+	err1 := ioutil.WriteFile("test.json", resp, 0644)
+
+	if err1 != nil {
+		fmt.Println("error:", err1)
+        c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
+            "message": "Unable to save the file",
+        })
+        return
+    }
+	
+	// File saved successfully. Return proper result
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Your file has been successfully saved."})
+
+
+	// TODO: POST body -> TBmapdata
+	/*req TBMapData
+	if err := c.ShouldBindJSON(&req); err != nil {
+		fmt.Println("error:", err)
+		return
+	}*/
+	//id, err := h.contentDB.AddTickBiteMapData(testInstanceID, testMapData)
 	// TODO: save TBmapdata into DB
 	// TODO: prepare response
 }
