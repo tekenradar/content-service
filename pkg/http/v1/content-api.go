eb2c4f614b904d3dcc1339c9f65d4fc84bd83dbc
phev8 <phev8@users.noreply.github.com>
2022-05-10 14:47:16 +0200

    run auto format

--- pkg/http/v1/content-api.go
+++ pkg/http/v1/content-api.go
@@ -7,7 +7,6 @@ import (
 	"strconv"
 	"time"
 
-
 	"github.com/gin-gonic/gin"
 	"github.com/tekenradar/content-service/pkg/types"
 )
@@ -19,12 +18,12 @@ func (h *HttpEndpoints) AddContentAPI(rg *gin.RouterGroup) {
 }
 
 func (h *HttpEndpoints) getTBReportMapDataHandl(c *gin.Context) {
+
 	//fetch data from DB
 	//date 4 weeks ago
-	t :=time.Now().AddDate(0,0,-28).Unix()
+	t := time.Now().AddDate(0, 0, -28).Unix()
 	testInstanceID := strconv.FormatInt(1650359785, 10)
-	points, err := h.contentDB.FindTickBiteMapDataByTime(testInstanceID,t)
-
+	points, err := h.contentDB.FindTickBiteMapDataByTime(testInstanceID, t)
 
 	//prepare response in TBReportMapData Format
 	if err != nil {
@@ -33,34 +32,34 @@ func (h *HttpEndpoints) getTBReportMapDataHandl(c *gin.Context) {
 		return
 	}
 
-	var tempReportMapData types.ReportMapData;
-	tempReportMapData.Slider.MinLabel = "-4 weken";
-	tempReportMapData.Slider.MaxLabel = "nu";
+	var tempReportMapData types.ReportMapData
+	tempReportMapData.Slider.MinLabel = "-4 weken"
+	tempReportMapData.Slider.MaxLabel = "nu"
 	tempReportMapData.Slider.Labels = make([]string, 4)
-	
+
 	timeFormat := "02-01-2006"
-	tempReportMapData.Slider.Labels[3] = time.Now().AddDate(0,0,-7).Format(timeFormat) + "  -  " + time.Now().Format(timeFormat)
-	tempReportMapData.Slider.Labels[2] = time.Now().AddDate(0,0,-14).Format(timeFormat) + "  -  " + time.Now().AddDate(0,0,-8).Format(timeFormat)
-	tempReportMapData.Slider.Labels[1] = time.Now().AddDate(0,0,-21).Format(timeFormat) + "  -  " + time.Now().AddDate(0,0,-15).Format(timeFormat)
-	tempReportMapData.Slider.Labels[0] = time.Now().AddDate(0,0,-28).Format(timeFormat) + "  -  " + time.Now().AddDate(0,0,-22).Format(timeFormat)
+	tempReportMapData.Slider.Labels[3] = time.Now().AddDate(0, 0, -7).Format(timeFormat) + "  -  " + time.Now().Format(timeFormat)
+	tempReportMapData.Slider.Labels[2] = time.Now().AddDate(0, 0, -14).Format(timeFormat) + "  -  " + time.Now().AddDate(0, 0, -8).Format(timeFormat)
+	tempReportMapData.Slider.Labels[1] = time.Now().AddDate(0, 0, -21).Format(timeFormat) + "  -  " + time.Now().AddDate(0, 0, -15).Format(timeFormat)
+	tempReportMapData.Slider.Labels[0] = time.Now().AddDate(0, 0, -28).Format(timeFormat) + "  -  " + time.Now().AddDate(0, 0, -22).Format(timeFormat)
 
 	tempReportMapData.Series = make([][]types.TickBiteMapData, 4)
 	for i := range points {
 
 		TempMapData := types.TickBiteMapData{
-			Lat : points[i].Lat,
-			Lng : points[i].Lng,
-			Type : points[i].Type,
+			Lat:  points[i].Lat,
+			Lng:  points[i].Lng,
+			Type: points[i].Type,
 		}
 		switch temp_t := points[i].Time; {
-		case temp_t < time.Now().AddDate(0,0,-21).Unix():
-			tempReportMapData.Series[0] = append(tempReportMapData.Series[0],TempMapData)
-		case temp_t < time.Now().AddDate(0,0,-14).Unix():
-			tempReportMapData.Series[1] = append(tempReportMapData.Series[1],TempMapData)
-		case temp_t < time.Now().AddDate(0,0,-7).Unix():
-			tempReportMapData.Series[2] = append(tempReportMapData.Series[2],TempMapData)
+		case temp_t < time.Now().AddDate(0, 0, -21).Unix():
+			tempReportMapData.Series[0] = append(tempReportMapData.Series[0], TempMapData)
+		case temp_t < time.Now().AddDate(0, 0, -14).Unix():
+			tempReportMapData.Series[1] = append(tempReportMapData.Series[1], TempMapData)
+		case temp_t < time.Now().AddDate(0, 0, -7).Unix():
+			tempReportMapData.Series[2] = append(tempReportMapData.Series[2], TempMapData)
 		default:
-			tempReportMapData.Series[3] = append(tempReportMapData.Series[3],TempMapData)
+			tempReportMapData.Series[3] = append(tempReportMapData.Series[3], TempMapData)
 		}
 	}
 
