c010f5cf62362727978b4baebb8682bb4881a900
Katja Schneider <katja.schneider@coneno.com>
2022-05-10 12:16:39 +0200

    GET handler: Fetch Data from DB and prepare response in ReportFormat

--- pkg/http/v1/content-api.go
+++ pkg/http/v1/content-api.go
@@ -1,7 +1,15 @@
 package v1
 
 import (
+	//"encoding/json"
+	"fmt"
+	"net/http"
+	"strconv"
+	"time"
+
+
 	"github.com/gin-gonic/gin"
+	"github.com/tekenradar/content-service/pkg/types"
 )
 
 func (h *HttpEndpoints) AddContentAPI(rg *gin.RouterGroup) {
@@ -11,8 +19,51 @@ func (h *HttpEndpoints) AddContentAPI(rg *gin.RouterGroup) {
 }
 
 func (h *HttpEndpoints) getTBReportMapDataHandl(c *gin.Context) {
-	// TODO: fetch data from DB
-	//t := time.Now() - 1561651651
-	//points, err = h.contentDB.FindTickBiteMapDataByTime(...)
-	// TODO: prepare response
+	//fetch data from DB
+	//date 4 weeks ago
+	t :=time.Now().AddDate(0,0,-28).Unix()
+	testInstanceID := strconv.FormatInt(1650359785, 10)
+	points, err := h.contentDB.FindTickBiteMapDataByTime(testInstanceID,t)
+
+
+	//prepare response in TBReportMapData Format
+	if err != nil {
+		fmt.Println("error:", err)
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "could not fetch data from db"})
+		return
+	}
+
+	var tempReportMapData types.ReportMapData;
+	tempReportMapData.Slider.MinLabel = "-4 weken";
+	tempReportMapData.Slider.MaxLabel = "nu";
+	tempReportMapData.Slider.Labels = make([]string, 4)
+	
+	timeFormat := "02-01-2006"
+	tempReportMapData.Slider.Labels[3] = time.Now().AddDate(0,0,-7).Format(timeFormat) + "  -  " + time.Now().Format(timeFormat)
+	tempReportMapData.Slider.Labels[2] = time.Now().AddDate(0,0,-14).Format(timeFormat) + "  -  " + time.Now().AddDate(0,0,-8).Format(timeFormat)
+	tempReportMapData.Slider.Labels[1] = time.Now().AddDate(0,0,-21).Format(timeFormat) + "  -  " + time.Now().AddDate(0,0,-15).Format(timeFormat)
+	tempReportMapData.Slider.Labels[0] = time.Now().AddDate(0,0,-28).Format(timeFormat) + "  -  " + time.Now().AddDate(0,0,-22).Format(timeFormat)
+
+	tempReportMapData.Series = make([][]types.TickBiteMapData, 4)
+	for i := range points {
+
+		TempMapData := types.TickBiteMapData{
+			Lat : points[i].Lat,
+			Lng : points[i].Lng,
+			Type : points[i].Type,
+		}
+		switch temp_t := points[i].Time; {
+		case temp_t < time.Now().AddDate(0,0,-21).Unix():
+			tempReportMapData.Series[0] = append(tempReportMapData.Series[0],TempMapData)
+		case temp_t < time.Now().AddDate(0,0,-14).Unix():
+			tempReportMapData.Series[1] = append(tempReportMapData.Series[1],TempMapData)
+		case temp_t < time.Now().AddDate(0,0,-7).Unix():
+			tempReportMapData.Series[2] = append(tempReportMapData.Series[2],TempMapData)
+		default:
+			tempReportMapData.Series[3] = append(tempReportMapData.Series[3],TempMapData)
+		}
+	}
+
+	c.JSON(http.StatusOK, tempReportMapData)
+
 }
--- pkg/types/map-data.go
+++ pkg/types/map-data.go
@@ -2,9 +2,24 @@ package types
 
 type TickBiteMapData struct {
 
-	Time int64   `bson:"time,omitempty"`
-	Lng  float64 `bson:"lng"`            //longitude
-	Lat  float64 `bson:"lat"`            //latitude
-	Type string  `bson:"type,omitempty"` //example: "TB"
+	Time int64   `bson:"time,omitempty" json:"time,omitempty"`
+	Lng  float64 `bson:"lng" json:"lng"` 			//longitude
+	Lat  float64 `bson:"lat" json:"lat"`            //latitude
+	Type string  `bson:"type,omitempty" json:"type,omitempty"` //example: "TB"
 	
+}
+
+type TimeSlider struct{
+
+	MinLabel string `json:"minLabel"`
+	MaxLabel string `json:"maxLabel"`
+	Labels[] string `json:"labels"`
+
+}
+
+
+type ReportMapData struct{
+
+	Slider	 TimeSlider 	`json:"slider"`
+	Series[][] TickBiteMapData `json:"series"`
 }
