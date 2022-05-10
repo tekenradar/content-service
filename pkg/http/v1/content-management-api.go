372f148ab8055d3860e69e58a812deca362d0f3d
phev8 <phev8@users.noreply.github.com>
2022-05-10 13:08:03 +0200

    add api key validation

--- pkg/http/v1/content-management-api.go
+++ pkg/http/v1/content-management-api.go
@@ -12,39 +12,37 @@ import (
 
 func (h *HttpEndpoints) AddContentManagementAPI(rg *gin.RouterGroup) {
 	data := rg.Group("/data")
-	// data.Use(mw.HasAPIKey(h.apiKeys.readWrite))
+	data.Use(mw.HasValidAPIKey(h.apiKeys.readWrite))
 	{
 		data.POST("/tb-report", mw.RequirePayload(), h.addTBReportHandl)
 	}
 }
 
 func (h *HttpEndpoints) addTBReportHandl(c *gin.Context) {
-	
 
 	// TODO: for debugging, save POST body as a JSON
 	resp, err := ioutil.ReadAll(c.Request.Body)
 
-    if err != nil {
-        c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
-            "message": "Unable to read request body",
-        })
-        return
-    }
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
+			"message": "Unable to read request body",
+		})
+		return
+	}
 
 	err1 := ioutil.WriteFile("test.json", resp, 0644)
 
 	if err1 != nil {
 		fmt.Println("error:", err1)
-        c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
-            "message": "Unable to save the file",
-        })
-        return
-    }
-	
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
+			"message": "Unable to save the file",
+		})
+		return
+	}
+
 	// File saved successfully. Return proper result
 	c.JSON(http.StatusOK, gin.H{
 		"message": "Your file has been successfully saved."})
-
 
 	// TODO: POST body -> TBmapdata
 	/*req TBMapData
