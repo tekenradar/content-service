1b035475f807d3b4f5d3e2ef70aa60474915a81a
Katja Schneider <katja.schneider@coneno.com>
2022-05-18 12:20:59 +0200

    take map data from study service object and add to db

--- pkg/http/v1/content-management-api.go
+++ pkg/http/v1/content-management-api.go
@@ -1,13 +1,16 @@
 package v1
 
 import (
-	"fmt"
-	"io/ioutil"
+	"strconv"
+	"strings"
 
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/influenzanet/study-service/pkg/studyengine"
+
 	mw "github.com/tekenradar/content-service/pkg/http/middlewares"
+	cstypes "github.com/tekenradar/content-service/pkg/types"
 )
 
 func (h *HttpEndpoints) AddContentManagementAPI(rg *gin.RouterGroup) {
@@ -20,37 +23,72 @@ func (h *HttpEndpoints) AddContentManagementAPI(rg *gin.RouterGroup) {
 
 func (h *HttpEndpoints) addTBReportHandl(c *gin.Context) {
 
-	// TODO: for debugging, save POST body as a JSON
-	resp, err := ioutil.ReadAll(c.Request.Body)
+	var load studyengine.ExternalEventPayload
 
+	if err := c.ShouldBindJSON(&load); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	// POST body -> TBmapdata
+	var TBdata cstypes.TickBiteMapData
+	TBdata.Time = load.Response.SubmittedAt
+
+	if strings.Contains(load.Response.Key, "TB") {
+		TBdata.Type = "TB"
+	} else if strings.Contains(load.Response.Key, "EM") {
+		TBdata.Type = "EM"
+	} else if strings.Contains(load.Response.Key, "Fever") {
+		TBdata.Type = "FE"
+	} else {
+		TBdata.Type = "Other"
+	}
+
+	//find map data in responses
+	//TODO: give error if element is not found
+	for i := range load.Response.Responses {
+		if strings.Contains(load.Response.Responses[i].Key, "TBLoc.Q2") {
+
+			for _, item := range load.Response.Responses[i].Response.Items {
+
+				if item.Key == "map" {
+					for _, mapItem := range item.Items {
+						if mapItem.Key == "lat" {
+							lat, err := strconv.ParseFloat(mapItem.Value, 64)
+							if err != nil {
+								//Correct handling of error here?
+								c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+							}
+							TBdata.Lat = lat
+
+						}
+						if mapItem.Key == "lng" {
+							lng, err := strconv.ParseFloat(mapItem.Value, 64)
+							if err != nil {
+								//Correct handling of error here?
+								c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+							}
+							TBdata.Lng = lng
+
+						}
+					}
+					break
+				}
+			}
+			break
+		}
+	}
+
+	// save TBmapdata into DB
+	_, err := h.contentDB.AddTickBiteMapData(load.InstanceID, TBdata)
 	if err != nil {
 		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
-			"message": "Unable to read request body",
-		})
+			"message": "Unable to add data to data base"})
 		return
 	}
 
-	err1 := ioutil.WriteFile("test.json", resp, 0644)
-
-	if err1 != nil {
-		fmt.Println("error:", err1)
-		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
-			"message": "Unable to save the file",
-		})
-		return
-	}
-
-	// File saved successfully. Return proper result
+	// prepare response
 	c.JSON(http.StatusOK, gin.H{
-		"message": "Your file has been successfully saved."})
+		"message": "Map Data successfully added to data base"})
 
-	// TODO: POST body -> TBmapdata
-	/*req TBMapData
-	if err := c.ShouldBindJSON(&req); err != nil {
-		fmt.Println("error:", err)
-		return
-	}*/
-	//id, err := h.contentDB.AddTickBiteMapData(testInstanceID, testMapData)
-	// TODO: save TBmapdata into DB
-	// TODO: prepare response
 }
