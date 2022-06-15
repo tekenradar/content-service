c51bde1e96b72ec3adb1fea9533e6f40f9d2cafa
Katja Schneider <katja.schneider@coneno.com>
2022-06-15 09:36:05 +0200

    define fields in json format

--- pkg/types/file-info.go
+++ pkg/types/file-info.go
@@ -4,9 +4,9 @@ import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type FileInfo struct {
 	ID         primitive.ObjectID `bson:"_id,omitempty" json:"id,omitempty"`
-	Path       string             `bson:"path,omitempty"`
-	UploadedAt int64              `bson:"uploadedAt,omitempty"`
-	FileType   string             `bson:"fileType,omitempty"`
-	Name       string             `bson:"name,omitempty"`
-	Size       int32              `bson:"size,omitempty"`
+	Path       string             `bson:"path,omitempty" json:"path,omitempty"`
+	UploadedAt int64              `bson:"uploadedAt,omitempty" json:"uploadedAt,omitempty"`
+	FileType   string             `bson:"fileType,omitempty" json:"fileType,omitempty"`
+	Name       string             `bson:"name,omitempty" json:"name,omitempty"`
+	Size       int32              `bson:"size,omitempty" json:"size,omitempty"`
 }
